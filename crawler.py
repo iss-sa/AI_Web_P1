d49476a6102ed8b90460d942cc89090bd4947b43
iss-sa <110250036+iss-sa@users.noreply.github.com>
2023-10-31 16:32:38 +0100

    pseudocode implementation try

--- crawler.py
+++ crawler.py
@@ -1,21 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
 
+first_URL = "https://vm009.rz.uos.de/crawl/index.html"
+stack_crawler = [first_URL]
+
+indexing_dic = {}
+
 # GET request 
-response = requests.get("http://info.cern.ch/hypertext/WWW/TheProject.html")
+#response = requests.get(stack_crawler.pop())
 #print(response.text)  # prints the request body (here: HTML content)
 #print(response.headers)
 #print(response.content)
 
-#ilvas versuch
 
-soup = BeautifulSoup(response.content, 'html.parser') # defualt parser html parser
+#soup = BeautifulSoup(response.content, 'html.parser') # defualt parser html parser
 #print(soup.title)
 #print(soup.title.text)
-print(soup.prettify())
+#print(soup.prettify())
 
-# to find all links (always start with <a ...)
+"""# to find all links (always start with <a ...)
 for l in soup.find_all("a"):
     #print(l)
     #print(l.text)
     print(l['href']) # extract URLs from link
+"""
+
+# while stack not empty
+while len(stack_crawler) != 0:
+    URL = stack_crawler.pop()
+    # If not visited frequently -> IMPLEMENTATION NEEDED ("update visited list")
+    response = requests.get(URL)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    for l in soup.find_all("a"):
+        stack_crawler.append(l['href']) # Invalid URL 'page3.html': No schema supplied.
+        print(l['href'])
+
+
+
