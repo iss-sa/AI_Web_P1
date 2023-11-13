4a06d4fb16ae816a52cf7dcacfd28c9b52a77682
ihovemann <ihovemann@uni-osnabrueck.de>
2023-11-13 10:42:39 +0100

    absolute URLs and visited list

--- crawler.py
+++ crawler.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 
 first_URL = "https://vm009.rz.uos.de/crawl/index.html"
 stack_crawler = [first_URL]
+visited_lst = []
 
 dickk = {"ilva": ["no dic"]}
 dickk['ilva'].append("nio")
@@ -11,47 +12,52 @@ print(lst)
 
 indexing_dic = {}
 
+
+
 # while stack not empty
 while len(stack_crawler) != 0:
     URL = stack_crawler.pop()
     # If not visited frequently -> IMPLEMENTATION NEEDED ("update visited list")
-    res = requests.get(URL)
-    soup = BeautifulSoup(res.content, "html.parser")
-    text = soup.find_all(text = True)
-    output = ''
-    blacklist = [
-        '[document]',
-        'noscript',
-        'header',
-        'html',
-        'meta',
-        'head', 
-        'input',
-        'script',
-    # name more elements if not required
-    ]
-    for t in text:
-        if t.parent.name not in blacklist:
-            output += '{} '.format(t)
+    if (URL not in visited_lst):
 
-    output_lst = output.split()
+        visited_lst.append(URL)
 
-    keys = []
-    for word in output_lst:
-        if word not in keys:
-            keys.append(word)
-        else:
-            continue
 
-    print(keys)
+        res = requests.get(URL, allow_redirects=False)
+        soup = BeautifulSoup(res.content, "html.parser")
+        text = soup.find_all(text = True)
+        output = ''
+        blacklist = [
+            '[document]',
+            'noscript',
+            'header',
+            'html',
+            'meta',
+            'head', 
+            'input',
+            'script',
+        # name more elements if not required
+        ]
+        for t in text:
+            if t.parent.name not in blacklist:
+                output += '{} '.format(t)
 
-    for key in keys:
-        if key not in indexing_dic.keys():
-            indexing_dic[key] = [URL]
-        else:
-            indexing_dic[key].append(URL)
-    
-    for l in soup.find_all("a"):
-        stack_crawler.append(l['href']) # Invalid URL 'page3.html': No schema supplied.
+        output_lst = output.split()
 
-        print(l['href'])
+        keys = []
+        for word in output_lst:
+            if word not in keys:
+                keys.append(word)
+            else:
+                continue
+
+        print(keys)
+
+        for key in keys:
+            if key not in indexing_dic.keys():
+                indexing_dic[key] = [URL]
+            else:
+                indexing_dic[key].append(URL)
+        
+        for l in soup.find_all("a"):
+            stack_crawler.append(requests.compat.urljoin('https://vm009.rz.uos.de/crawl/index.html', l['href'])) # Invalid URL 'page3.html': No schema supplied.
